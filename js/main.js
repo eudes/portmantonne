8a38475d3aa158415ac04106dda922f6e2f95ea0
eudes <1531268+eudes@users.noreply.github.com>
2022-04-24 19:27:47 +0100

    Sort results by pronounceability

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 const {portmantonne, csvToArray} = require("./lib");
+const {Pronounceable} = require("@eudes/pronounceable");
+let pronounceable = new Pronounceable();
 
 const listener = (event) => {
   const portmanteaus = portmantonne(
     csvToArray(event.target.value, ',')
-  )
+  ).map(w => ({word: w, score: pronounceable.score(w)}))
+    .sort((a, b) => b.score - a.score)
   const outputElement = document.getElementById('output')
   outputElement.innerHTML = ''
   portmanteaus.forEach(word => {
     const newEl = document.createElement('p');
-    newEl.textContent = word
+    newEl.textContent = word.word
     outputElement.appendChild(newEl)
   })
 }
